7f8cb36a374759061421d25b681727e12f67e03a
Kamil Machul <kamil.machul@gmail.com>
2020-08-18 22:49:41 +0200

    Add welcome screen

---
+++ app/src/main/java/com/km/mbottlecapcollector/WelcomeScreenActivity.java
@@ -0,0 +1,24 @@
+package com.km.mbottlecapcollector;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class WelcomeScreenActivity extends Activity {
+    private Handler handler;
+    private int delayInSeconds = 3;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+
+        handler = new Handler();
+        handler.postDelayed(() -> {
+            Intent intent = new Intent(WelcomeScreenActivity.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        }, delayInSeconds * 1000);
+
+    }
+}
